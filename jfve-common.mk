d98a9f40c94b1baafa7ee58bf957727389f4e223
arco <arco68@gmail.com>
2019-03-07 11:42:16 +0100

    Import USB from serrano-common

--- jfve-common.mk
+++ jfve-common.mk
@@ -285,7 +285,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.jf
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
